e2e5fedd1378efd63b9901203ccc1312902fd13a
Dmitrii Shilkin <dmitriishilkin@gmail.com>
2023-11-15 13:51:57 +0300

    Доработаны модель, сериализатор и вью для Post Доработаны модели Review, Comment, Category Добавлена модель Ip Созданы сериализаторы и вью для Review, Comment, Category В settings.py локализация переключена на ru-ru Обновлен requirements.txt Обновлены миграции для приложения posts Добавлены url для reviews, comments, categories В админку добавлена модель Ip

--- echo/posts/models.py
+++ echo/posts/models.py
@@ -1,8 +1,17 @@
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import Avg, Q
 
 from profile.models import Profile
 from profile.models import get_image_path
+
+
+# Модель для айпи адресов
+class Ip(models.Model):
+    ip = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ip
 
 
 # Модель для категорий
@@ -17,34 +26,114 @@ class Category(models.Model):
 # Модель для публикаций
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    published = models.DateTimeField(blank=True, null=True)
     modified = models.DateTimeField(blank=True, null=True)
+    headline = models.CharField(verbose_name='Заголовок', max_length=254)
     text = models.TextField(
         validators=[
             MinLengthValidator(50, 'Это поле должно содержать минимум 50 символов')
         ],
-        verbose_name='Публикация',
+        verbose_name='Содержание',
     )
     image = models.ImageField(blank=True, null=True, verbose_name='Изображение', upload_to=get_image_path)
+    is_published = models.BooleanField(default=False)
+    rating = models.FloatField(default=0.0)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    favourite = models.ManyToManyField(Profile, blank=True, related_name='favourites')
+    views = models.ManyToManyField(Ip, related_name='post_views', blank=True)
+    favourite = models.ManyToManyField(Profile, related_name='favourite_posts', blank=True)
     category = models.ManyToManyField(Category, related_name='posts', verbose_name='Категория')
 
+    class Meta:
+        ordering = ('-published', '-created')
+
     def __str__(self):
-        return self.text[:50]
+        return self.headline.capitalize()
+
+    def update_rating(self):
+        # получаем все отзывы на данную публикацию
+        reviews = Review.objects.filter(post_id=self.pk)
+        # вычисляем среднее арифметическое рейтинга всех отзывов на данную публикацию
+        self.rating = reviews.aggregate(Avg('rating'))['rating__avg']
+        self.save()
+
+    def total_views(self):
+        return self.views.count()
+
+    # def total_likes(self, value):
+    #     likes = Like.objects.filter(Q(post_id=self.pk) & Q(value=value)).count()
+    #
+    #     return likes
+
+    def total_comments(self):
+        comments = Comment.objects.filter(post_id=self.pk).count()
+
+        return comments
+
+    def set_published(self):
+        self.is_published = True
+        self.save()
+
+
+# Модель для отзывов
+class Review(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(blank=True, null=True)
+    headline = models.CharField(verbose_name='Заголовок', max_length=254)
+    text = models.TextField(
+        validators=[
+            MinLengthValidator(20, 'Это поле должно содержать минимум 20 символов')
+        ],
+        verbose_name='Содержание',
+    )
+    rating = models.IntegerField(verbose_name='Оценка', validators=[MinValueValidator(1), MaxValueValidator(5)])
+    image = models.ImageField(blank=True, null=True, verbose_name='Изображение', upload_to=get_image_path)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-created', )
+
+    def __str__(self):
+        return self.headline.capitalize()
 
 
 # Модель для комментариев
 class Comment(models.Model):
-    text = models.CharField(max_length=254, blank=True, verbose_name='Комментарий')
+    text = models.CharField(max_length=254, verbose_name='Комментарий')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(blank=True, null=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-created', )
+
+    def __str__(self):
+        return self.text[:20]
 
 
 # Модель для лайков
 class Like(models.Model):
-    value = models.IntegerField(default=0)
+    value = models.IntegerField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    # def plus_like(self):
+    #     if self.value is None or self.value == 0:
+    #         self.value = 1
+    #
+    #     if self.value == 1:
+    #         self.value = 0
+    #
+    #     return self.value
+    #
+    # def dislike(self):
+    #     if self.value is None or self.value == 0:
+    #         self.value = -1
+    #
+    #     if self.value == -1:
+    #         self.value = 0
+    #
+    #     return self.value
--- echo/posts/serializers.py
+++ echo/posts/serializers.py
@@ -1,97 +1,146 @@
 from rest_framework import serializers
-from drf_writable_nested import WritableNestedModelSerializer
+# from drf_writable_nested import WritableNestedModelSerializer
 
-from .models import Post, Category, Comment, Like
+from .models import Post, Category, Comment, Like, Review
 
 
 class CategorySerializer(serializers.ModelSerializer):
-    name = serializers.CharField(validators=[])
 
     class Meta:
         model = Category
         fields = [
+            'url',
+            'id',
             'name',
         ]
-        # отключаем встроенный валидатор, чтобы не было ошибки уникальности имен,
-        # т.к. при создании публикации мы не создаем новые категории, а берем имеющиеся
-        # extra_kwargs = {
-        #     'name': {
-        #         'validators': []
-        #     },
-        # }
-
-    def save(self, **kwargs):
-        self.is_valid()
-        name = self.validated_data.get('name')
-        category = Category.objects.filter(name=name)
-        if category.exists():
-            return category.first()
-
-    # def create(self, validated_data):
-    #     pass
-
-    # def validate_name(self, value):
-    #     category = Category.objects.filter(name=value)
-    #     if not category.exists():
-    #         raise serializers.ValidationError(f'Указано имя несуществующей категории {value}')
-    #
-    #     return value
-
-    # def get_choices(self, request):
-    #     queryset = Category.objects.all()
-    #     choices = {}
-    #     for q in queryset:
-    #         choices[q] = q
-    #     return choices
 
 
-# сериализатор модели публикаций
-class PostSerializer(WritableNestedModelSerializer):
-    # category = serializers.ManyRelatedField(child_relation=CategorySerializer())
-    # category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True)
-    category = CategorySerializer(many=True, required=True)
-    image = serializers.URLField(label='URL', allow_blank=True)
+# Cериализатор модели публикаций
+class PostSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True, allow_empty=False)
+    image = serializers.URLField(label='Image URL', allow_blank=True)
 
     class Meta:
         model = Post
         depth = 1
         fields = [
+            'url',
             'id',
+            'headline',
             'text',
             'image',
             'category',
         ]
 
-    # проверяем имя переданной категории, преобразуем OrderedDict с именами категорий в список объектов типа "Категория"
-    # и возвращаем его
-    def validate_category(self, value):
-        categories = []
-
-        if value:
-            for val in value:
-                # достаем имя каждой категории из OrderedDict
-                category_name = list(val.items())[0][1]
-                # проверяем есть ли категория с указанным именем
-                category = Category.objects.filter(name=category_name).first()
-
-                if not category:
-                    raise serializers.ValidationError(f"Указано имя несуществующей категории '{category_name}'")
-
-                categories.append(category)
-
-            return categories
-
-    # переопредяем метод post
+    # переопредяем метод post, чтобы передать авторизованного пользователя и добавить связь со многими категориями
     def create(self, validated_data, **kwargs):
         # получаем данные публикации из валидатора
         categories = validated_data.pop('category')
         user = validated_data.pop('user')
+        headline = validated_data.pop('headline')
         text = validated_data.pop('text')
         image = validated_data.pop('image')
         # создаем публикацию
-        post = Post.objects.create(text=text, image=image, author=user)
+        post = Post.objects.create(headline=headline, text=text, image=image, author=user)
         # добавляем связи с категориями
         if categories:
             post.category.set(categories)
 
         return post
+
+
+# Сериализатор модели отзывов
+class ReviewSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+    image = serializers.URLField(label='image URL', allow_blank=True)
+
+    class Meta:
+        model = Review
+        depth = 1
+        fields = [
+            'url',
+            'id',
+            'headline',
+            'text',
+            'rating',
+            'image',
+            'post',
+        ]
+
+    # переопредяем метод post, чтобы передать авторизованного пользователя
+    def create(self, validated_data, **kwargs):
+        # получаем данные отзыва из валидатора
+        user = validated_data.pop('user')
+        review = Review.objects.create(**validated_data, user=user)
+
+        return review
+
+
+# Сериализатор модели комментариев
+class CommentSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all(), allow_null=True)
+    review = serializers.PrimaryKeyRelatedField(queryset=Review.objects.all(), allow_null=True)
+
+    class Meta:
+        model = Comment
+        depth = 1
+        fields = [
+            'url',
+            'id',
+            'text',
+            'post',
+            'review',
+        ]
+
+    # переопредяем метод post, чтобы передать авторизованного пользователя
+    def create(self, validated_data, **kwargs):
+        # получаем данные отзыва из валидатора
+        user = validated_data.pop('user')
+        text = validated_data.pop('text')
+        post = validated_data.pop('post')
+        review = validated_data.pop('review')
+
+        # если передана связь с моделью post, создаем комментарий к post
+        if post and not review:
+            comment = Comment.objects.create(text=text, user=user, post=post)
+            return comment
+
+        # если передана связь с моделью review, создаем комментарий к review
+        if review and not post:
+            comment = Comment.objects.create(text=text, user=user, review=review)
+            return comment
+
+        # если не передано ни одной связи с моделью
+        if not (post or review):
+            raise serializers.ValidationError('Поля post и review не могут быть пустыми одновременно.')
+
+        # если передана связь сразу на обе модели
+        if post and review:
+            raise serializers.ValidationError('Поля post и review не могут быть заполнены одновременно.')
+
+    # переопределяем метод patch, чтобы выполнить проверки связей с моделями
+    def update(self, instance, validated_data):
+        instance.text = validated_data.get("text", instance.text)
+        instance.post = validated_data.get("post", instance.post)
+        instance.review = validated_data.get("review", instance.review)
+
+        # если не передано ни одной связи с моделью
+        if not (instance.post or instance.review):
+            raise serializers.ValidationError('Поля post и review не могут быть пустыми одновременно.')
+
+        # если передана связь сразу на обе модели
+        if instance.post and instance.review:
+            raise serializers.ValidationError('Поля post и review не могут быть заполнены одновременно.')
+
+        instance.save()
+
+        return instance
+
+
+# Сериализатор модели лайков
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = [
+            'value',
+        ]
